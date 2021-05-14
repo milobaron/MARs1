183ede01aee7138a2842c3a1b90356dec3578fc3
milobaron <mikeveson@icloud.com>
2021-05-13 22:06:45 -0400

    testrohan

--- MARRs1/MARRs1/ViewController.swift
+++ MARRs1/MARRs1/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController, VoiceOverlayDelegate, YTPlayerViewDelega
             if final {
                 print("Final text: \(text)")
                 let userFrases = "\(text)"
-                
+                let word = "rohan is dumb"
                 self.playerView.load(withVideoId: "oKtrHy0ERNA", playerVars: ["playsinline" : 1])
             }
                 
