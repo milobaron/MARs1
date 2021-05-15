7abb0eb52e381ea9a935971e38034b92c1c16f67
milobaron <mikeveson@icloud.com>
2021-05-14 22:56:59 -0400

    many changes

--- MARRs1/MARRs1/ViewController.swift
+++ MARRs1/MARRs1/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController, VoiceOverlayDelegate, YTPlayerViewDelega
     let voiceOverlay = VoiceOverlayController()
     @IBOutlet var playerView: YTPlayerView!
     @IBOutlet var button: UIButton!
+    @IBOutlet var button1: UIButton!
     @IBOutlet weak var tblVideos: UITableView!
     @IBOutlet weak var segDisplayedContent: UISegmentedControl!
     @IBOutlet weak var viewWait: UIView!
@@ -40,24 +41,15 @@ class ViewController: UIViewController, VoiceOverlayDelegate, YTPlayerViewDelega
     
     override func viewDidLoad(){
         super.viewDidLoad()
-        playerView.delegate = self
-        let button1 = UIButton(frame: CGRect(x: 75, y: 50, width: 250, height: 50))
-        view.addSubview(button1)
-        button1.backgroundColor = .white
-        button1.center = view.center
-        button1.setTitle("Show tab bar", for: .normal)
-        button1.setTitleColor(.black, for: .normal)
-        button1.addTarget(self, action: #selector(didPressButton), for: .touchUpInside)
-        view.backgroundColor = .init(red: 0.114, green: 0.314, blue: 0.819, alpha: 1)
-        playerView.load(withVideoId: "1InIKzeGKtY", playerVars: ["playsinline" : 1])
-        button.backgroundColor = .white
-        button.setTitleColor(.black, for: .normal)
+//        playerView.delegate = self
+        button1.roundCorners()
+//        playerView.load(withVideoId: "1InIKzeGKtY", playerVars: ["playsinline" : 1])
     }
     func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
         playerView.playVideo()
     }
     // did press button method for tab bar
-    @objc func didPressButton(){
+    @IBAction func didPressButton(){
         let tabBarVC = CustomTabBarController()
         present(tabBarVC, animated: true)
     }
@@ -72,8 +64,6 @@ class ViewController: UIViewController, VoiceOverlayDelegate, YTPlayerViewDelega
             if final {
                 print("Final text: \(text)")
                 let userWords = "\(text)"
-                // _S7r9MCc2ts
-//                let vidId = self.searchByKeyword(s: userWords)
                 self.playerView.load(withVideoId: "_S7r9MCc2ts", playerVars: ["playsinline" : 1])
             }
                 
@@ -99,12 +89,15 @@ class CustomTabBarController: RAMAnimatedTabBarController{
         let vc2 = UIViewController()
         let vc3 = UIViewController()
         let vc4 = UIViewController()
+        let vc5 = UIViewController()
+        
         vc1.view.backgroundColor = .init(red: 18/255.0, green: 190/255.0, blue: 93/255.0, alpha: 1.0)
         vc2.view.backgroundColor = .init(red: 234/255.0, green: 202/255.0, blue: 46/255.0, alpha: 1.0)
         vc3.view.backgroundColor = .systemRed
         vc4.view.backgroundColor = .init(red: 86/255.0, green: 158/255.0, blue: 250/255.0, alpha: 1.0)
+        vc5.view.backgroundColor = .green
         
-        vc1.tabBarItem = RAMAnimatedTabBarItem(title: "Home", image: UIImage(systemName: "house" ), tag: 1)
+        vc1.tabBarItem = RAMAnimatedTabBarItem(title: "Stocks", image: UIImage(systemName: "chart.bar.xaxis" ), tag: 1)
         (vc1.tabBarItem as? RAMAnimatedTabBarItem)?.animation = RAMBounceAnimation()
         
         vc2.tabBarItem = RAMAnimatedTabBarItem(title: "Crypto", image: UIImage(systemName: "bitcoinsign.circle" ), tag: 1)
@@ -115,7 +108,18 @@ class CustomTabBarController: RAMAnimatedTabBarController{
         
         vc4.tabBarItem = RAMAnimatedTabBarItem(title: "Weather", image: UIImage(systemName: "cloud" ), tag: 1)
         (vc4.tabBarItem as? RAMAnimatedTabBarItem)?.animation = RAMBounceAnimation()
-        setViewControllers([vc1, vc2, vc3, vc4], animated: false)
+
+        vc5.tabBarItem = RAMAnimatedTabBarItem(title: "Music", image: UIImage(systemName: "music.note" ), tag: 1)
+        (vc5.tabBarItem as? RAMAnimatedTabBarItem)?.animation = RAMBounceAnimation()
+
+        setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: false)
         
     }
+}
+public extension UIView {
+  //Round the corners
+  func roundCorners(){
+    let radius = bounds.maxX / 16
+    layer.cornerRadius = radius
+  }
 }
