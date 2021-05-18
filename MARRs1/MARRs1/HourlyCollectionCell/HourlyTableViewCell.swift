aa288cabd3e00ff9ba29814dfc53f40876a510a1
milobaron <mikeveson@icloud.com>
2021-05-18 17:53:54 -0400

    fixed some things with adaptation, ready for further building

---
+++ MARRs1/MARRs1/HourlyCollectionCell/HourlyTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HourlyTableViewCell.swift
+//  MARRs1
+//
+//  Created by Mike Veson on 5/18/21.
+//
+
+import UIKit
+
+class HourlyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    static let identifier = "HourlyTableViewCell"
+    static func nib() -> UINib {
+        return UINib(nibName: "HourlyTableViewCell", bundle: nil)
+    }
+}
